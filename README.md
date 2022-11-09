bacd82300568e7f6afed22234e0aa85fd0ae6ff2
Santiago Segre <110129047+santiagosegre@users.noreply.github.com>
2022-11-09 19:43:53 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,46 @@
+<h1 align="center"> Calendário de Jogos da Copa 2022 </h1>
+
+<p align="center">
+Evento exclusivo e gratuito, promovido pela Rocketseat para ensino de tecnologias WEB.
+</p>
+
+<p align="center">
+  <a href="#-tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-projeto">Projeto</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-layout">Layout</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#memo-licença">Licença</a>
+</p>
+
+<p align="center">
+  <img alt="License" src="https://img.shields.io/static/v1?label=license&message=MIT&color=49AA26&labelColor=000000">
+</p>
+
+<br>
+
+<p align="center">
+  <img alt="calendário da copa" src="./github/projeto.png" width="100%">
+</p>
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- HTML e CSS
+- JavaScript
+- Git e GitHub
+
+## 💻 Projeto
+
+Calendário da Copa é um projeto que mostra os jogos da Copa do Mundo de 2022.
+
+## 🔖 Layout
+
+Você pode visualizar o layout do projeto através [DESSE LINK](https://www.figma.com/file/21IkghridrJRcbQQ6EMxVn/Calend%C3%A1rio-de-Jogos-(Community)?node-id=0%3A1). É necessário ter conta no [Figma](https://figma.com) para acessá-lo.
+
+## :memo: Licença
+
+Esse projeto está sob a licença MIT.
+
+---
+
+Feito por [Santiago Segre](https://github.com/santiagosegre) com a ajuda da Rocketseat :wave: [Participe da nossa comunidade!](https://discord.gg/rocketseat)
---
+++ js/main.js
@@ -0,0 +1,77 @@
+
+function createMenu(icon, menu) {
+  return `
+<ul>
+  <a href="./pages/groups.html">
+    <li>
+      <i class="${icon}"></i>
+      <h3>${menu}</h3>
+    </li>
+  </a>
+</ul>
+`
+}
+
+document.querySelector("#menu-options").innerHTML = 
+createMenu(
+  "ri-table-line",
+  "Grupos"
+) +
+createMenu(
+  "ri-calendar-event-line",
+  "Calendário"
+)
+
+let delay = -0.5
+function createTeam(team, nameTeam) {
+  delay = delay + 0.2
+  return `
+<ul >
+    <a href="./pages/${team}.html">
+    <li class="teams" style="animation-delay: ${delay}s">
+      <img src="assets/teams/icon-${team}.svg" alt="">
+      <p>
+        ${nameTeam}
+      </p>
+     </li>
+    </a>
+</ul>
+`
+}
+
+// team: Nome do arquivo que tem a imagem
+// nameTeam: Como vai aparecer o nome da Seleção no <p>
+
+document.querySelector("#menu-teams").innerHTML =
+  createTeam("qatar", "Qatar") +
+  createTeam("ecuador", "Equador") +
+  createTeam("senegal", "Senegal") +
+  createTeam("netherlands", "Holanda") +
+  createTeam("england", "Inglaterra") +
+  createTeam("iran", "Irã") +
+  createTeam("united-states", "Estados unidos") +
+  createTeam("wales", "País de Gales") +
+  createTeam("argentina", "Argentina") +
+  createTeam("saudi-arabia", "Arábia Saudita") +
+  createTeam("mexico", "México") +
+  createTeam("poland", "Polônia") +
+  createTeam("france", "França") +
+  createTeam("denmark", "Dinamarca") +
+  createTeam("tunisia", "Tunísia") +
+  createTeam("australia", "Austrália") +
+  createTeam("spain", "Espanha") +
+  createTeam("germany", "Alemanha") +
+  createTeam("japan", "Japão") +
+  createTeam("costa-rica", "Costa Rica") +
+  createTeam("belgium", "Bélgica") +
+  createTeam("canada", "Canadá") +
+  createTeam("morocco", "Marrocos") +
+  createTeam("croatia", "Croácia") +
+  createTeam("brazil", "Brasil") +
+  createTeam("serbia", "Sérvia") +
+  createTeam("switzerland", "Suíça") +
+  createTeam("cameroon", "Camarões") +
+  createTeam("portugal", "Portugal") +
+  createTeam("ghana", "Gana") +
+  createTeam("uruguay", "Uruguai") +
+  createTeam("south-korea", "Korea do Sul")
